9228bbd0c1645b1bf3b4b1a803311b3697f84209
malin7yt <MALIN7YT@GMAIL.COM>
2022-07-10 19:43:07 -0400

    Imported loader spinner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import { Puff } from  'react-loader-spinner';
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-BAJS
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+WEATHER REACT APP
+        </h1>
+        <Puff
+    height="200"
+    width="200"
+    color='orange'
+    ariaLabel='loading'
+  />
+        <h4>In progress...</h4>
       </header>
     </div>
   );
