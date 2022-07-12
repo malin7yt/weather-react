e6a4fff27541affb4202b0a75775a5ebf9ee6a06
malin7yt <MALIN7YT@GMAIL.COM>
2022-07-12 16:36:57 -0400

    Updated signature links

--- src/Signature.js
+++ src/Signature.js
@@ -5,14 +5,19 @@ import "./Signature.css"
 export default function Signature() {
     return (
         <div className="signature">
-                <p>
+                <p>This project was coded by 
+            <a 
+            href="https://malinjensen.com/"
+            className="signature-link"
+            target="_blank"
+            rel="noopener noreferrer"> Malin Jensen</a> and is {" "}
             <a 
             href="https://github.com/malin7yt/weather-react" 
             className="signature-link" 
             target="_blank" 
             rel="noopener noreferrer">
-                Open source Code
-            </a> by Malin Jensen, hosted on {" "}  
+                open-sourced on Github, 
+            </a> hosted on {" "}  
             <a 
             href="https://keen-boba-597d50.netlify.app/" 
             className="signature-link" 
