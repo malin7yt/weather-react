434e9f9879baf0f382826d1b975359951421c679
malin7yt <MALIN7YT@GMAIL.COM>
2022-07-13 21:28:31 -0400

    Updated css, deleted components and moved code to primary comp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Weather from "./Weather"
-import Signature from './Signature';
 
 import "./App.css";
 
@@ -8,11 +7,33 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <div className="full-app">
         <div className="app-content">
       <Weather defaultCity="New York"/>
-    </div>
-    <Signature />
+
+      <div className="signature mt-5">
+      <footer>
+            This project was coded by 
+            <a 
+            href="https://malinjensen.com/"
+            className="signature-link"
+            target="_blank"
+            rel="noopener noreferrer"> Malin Jensen</a> and is {" "}
+            <a 
+            href="https://github.com/malin7yt/weather-react" 
+            className="signature-link" 
+            target="_blank" 
+            rel="noopener noreferrer">
+                open-sourced on Github, 
+            </a> hosted on {" "}  
+            <a 
+            href="https://keen-boba-597d50.netlify.app/" 
+            className="signature-link" 
+            target="_blank" 
+            rel="noopener noreferrer">
+                 Netlify
+            </a>
+        </footer>
+        </div>
     </div>
     </div>
     </div>
